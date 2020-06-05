7bef5a9a8fcf15acbdb3e6fac1deeae507824725
Wail Farhat <Wail Farhat@DESKTOP-QP3P64P>
2020-06-05 10:33:52 +0200

    Zakariaa Marrah First Commit

---
+++ sos-iam-stuck/src/main/java/com/formationjee/sosiamstuck/SosIamStuckApplication.java
@@ -0,0 +1,13 @@
+package com.formationjee.sosiamstuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosIamStuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosIamStuckApplication.class, args);
+	}
+
+}
---
+++ sos-iam-stuck/src/test/java/com/formationjee/sosiamstuck/SosIamStuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formationjee.sosiamstuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosIamStuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
